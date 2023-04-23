0a2fcf4f723a09baa15d4a398fef07f2a26a951f
Amanda Franklin-Ryan <amanda.franklin.ryan@gmail.com>
2023-04-23 21:23:53 +0200

    Add bombed sites to interactive map

--- R Scripts/Interactive Raqqa Mmap.R
+++ R Scripts/Interactive Raqqa Mmap.R
@@ -1,19 +1,32 @@
 library(leaflet)
 library(dplyr)
+library(rio)
 
 ###--- Create interactive leaflet map of Raqqa
-
 #Coordinates centre of Raqqa from average of each column in UNOSAT data
 #Calculated in Exploratory Data Analysis UNOSAT file
 
-leaflet()%>% 
+#Possible lap tile options: CartoDB, Esri, OpenStreetMap
+map_raqqa <- leaflet()%>% 
   addProviderTiles("CartoDB") %>% 
   
 # Set centre of map
-setView(lng=39.00897, lat= 35.95394, zoom=13) %>% #centred on Raqqa from UNOSAT data
+setView(lng=39.00897, lat= 35.95394, zoom=14) %>% #centred on Raqqa from UNOSAT data
   
 # Set maximum map bounds, calculated for max and min bounds of UNOSAT data
 setMaxBounds(lng1 = 39.05947, 
                lat1 = 35.9745, 
                lng2 = 38.93641, 
                lat2 = 35.92829)
+
+###--- Add locations of bombed buildings from October 2017 Coalition bombing
+
+unosat_data <- rio::import("Datasets/Reshaped UNOSAT data.rds")
+
+coalition_strikes_2017 <- unosat_data %>% 
+  filter(damage_year=="2017/10/21")
+
+map_raqqa_annotated <-  map_raqqa %>% 
+  clearMarkers() %>%  #clear large pin markers
+  addCircleMarkers(lng=coalition_strikes_2017$coords.x1, lat=coalition_strikes_2017$coords.x2,
+             radius = 0.1, color = "black", popup = NULL)
