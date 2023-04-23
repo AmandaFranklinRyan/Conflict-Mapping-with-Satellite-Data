28500a1310eb92ea649cf38c8fec6067a58fe134
Amanda Franklin-Ryan <amanda.franklin.ryan@gmail.com>
2023-04-23 16:41:27 +0200

    Types of buildings destroyed coalition bombing

--- R Scripts/Exploratory Data Analysis UNOSAT.R
+++ R Scripts/Exploratory Data Analysis UNOSAT.R
@@ -1,6 +1,8 @@
 library(dplyr)
 library(ggplot2)
 library(lubridate)
+library(data.table)
+library(formattable)
 
 #Load UNOSAT data in dataframe form
 raqqa_data <- readRDS("Datasets/Reshaped UNOSAT data.rds")
@@ -11,21 +13,19 @@ raqqa_data_summarised <- raqqa_data %>%
   select(SiteID,Neigh,damage_year,destruction_type,coords.x1,coords.x2) %>% 
   mutate(damage_year=lubridate::ymd(damage_year))
 
-### --- Exploratory Data Analysis
+### --- Exploratory Data Analysis---
 
 #Total number of buildings destroyed
 nrow(raqqa_data_summarised)
 # 13155 buildings destroyed over 5 year period
 
-#When were most buildings destroyed or damaged?
+### 1. When were most buildings destroyed or damaged?
 no_building_destroyed <- raqqa_data_summarised %>% 
   group_by(damage_year, destruction_type) %>% 
   summarise(n())
 
 #Unsurprisingly most damage occurred during coalition bombing in October 2017
-# Over 100000 buildings damaged
-
-colnames(no_building_destroyed)
+# Over 100000 buildings damaged during this time
 
 # Bar chart illustrating severity of damage by date
 year_labels <- c("October 2013","February 2014","May 2015","February 2017", "October 2017")
@@ -37,21 +37,30 @@ damage_plot <- ggplot(data=no_building_destroyed, aes(x=damage_year, y=`n()`, fi
   scale_x_discrete( labels= year_labels)+
   ggtitle("Severity of damage to buildings in Raqqa (2013-2017)")
   
-scale_x_discrete(name, breaks, labels, limits)
+###2. What kind of buildings were destroyed?
+
+# Calculate type of buildings destroyed by year
+buildings_destroyed <- raqqa_data_summarised %>% 
+  group_by(damage_year,SiteID) %>% 
+  summarise(n()) %>% 
+  arrange(n()) %>% 
+  ungroup()
+
+# Focusing exclusively on the coalition bombing of October 2017
+coalition_bombing <- buildings_destroyed %>% 
+  filter(damage_year== "2017-10-21") %>% 
+  arrange(desc(`n()`)) 
+
+names(coalition_bombing) <- c('Year','Building type','Number')
+
+coalition_bombing <- coalition_bombing %>% 
+  select(`Building type`, Number)
+
+coalition_bombing_data_table <- as.data.table(keywords_by_date)
+formatted_table <- formattable(keywords_data_table, 
+                               align=c("l","l"))
 
 
-str(no_building_destroyed)
-
-#1. What kind of buildings were destroyed?
-
-# Select destruction of columns by date showing all results where at least 3 buildings of that type have been destroyed
-buildings_destroyed <- destroyed_no_nas %>% 
-  select(SiteID, Neigh, destroyed_year) %>% 
-  group_by(destroyed_year,SiteID) %>% 
-  summarise(sum(total=n())) %>% 
-  arrange(sum(total=n())) %>% 
-  ungroup() %>% 
-  dplyr::filter(sum(total=n()) > 5)
 
 top_buildings_destroyed <- dplyr::rename(buildings_destroyed, total=`sum(total=n())`)
 
